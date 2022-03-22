8f0e73d2714f8f58022f45fd5c26b421c2f9488e
victoria-dk <victoria@hotmail.com>
2022-03-22 16:01:41 +0100

    Add styling and edit links in db

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from typing import List, Dict
-from flask import Flask, request
+from flask import Flask, request, render_template
 import mysql.connector
 import json
 
@@ -32,6 +32,22 @@ def add_item(title, link, descript):
     connection.close()
     return request
 
+def delete_item(title):
+    connection = mysql.connector.connect(**DB_conf)
+    cursor = connection.cursor()
+    request = f"DELETE FROM images WHERE title = '{title}';"
+    cursor.execute(request)
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return request
+
+def style():
+    S  = "<style>\n"
+    S += " body {background-color: rgb(189, 160, 216);} table {width: 50%; height:50%; background-color: white; border-radius: 2%; width: 70%; padding: 30px; margin: 20px;} table, th, td {border-collapse: collapse; padding: 10px; border: 5px solid yellowg}\n"
+    S += "</style>\n"
+    return S
+
 @app.route('/add')
 def add():
     title = request.args.get("title", "", str)
@@ -41,6 +57,7 @@ def add():
     S += "<html>\n"
     S += "  <head>\n"
     S += "      <title>Added a link</title>\n"
+    S += style()
     S += "  </head>\n"
     S += "  <body>\n"
     S += "      <h1>Added a link</h1>\n"
@@ -51,12 +68,109 @@ def add():
     S += "</html>\n"
     return S
 
+@app.route('/delete')
+def delete():
+    title = request.args.get("title", "", str)
+    S  = "<!DOCTYPE html>\n"
+    S += "<html>\n"
+    S += "  <head>\n"
+    S += "      <title>Deleted a link</title>\n"
+    S += style()
+    S += "  </head>\n"
+    S += "  <body>\n"
+    S += "      <h1>Deleted a link</h1>\n"
+    if title != "":
+        S += delete_item(title)
+    S += "      <p><a href='/'>Back!</a></p>\n"
+    S += "  </body>\n"
+    S += "</html>\n"
+    return S
+
+@app.route('/addform')
+def addform():
+    S  = "<!DOCTYPE html>\n"
+    S += "<html>\n"
+    S += "  <head>\n"
+    S += "      <title>Entering a value</title>\n"
+    S += style()
+    S += "  </head>\n"
+    S += "  <body>\n"
+    S += "      <h1>Entering a value</h1>\n"
+    S += "      <form action='/add'>\n"
+    S += "          <input type='text' name='title' value='Affenpinscher'/>\n"
+    S += "          <input type='text' name='link' value='https://upload.wikimedia.org/wikipedia/commons/2/25/Affenpinscher_dog.jpg'/>\n"
+    S += "          <input type='text' name='descript' value='Affenpinscher dog'/>\n"
+    S += "          <input type='submit' value='Submit'/>\n"
+    S += "      </form>\n"
+    S += "  </body>\n"
+    S += "</html>\n"
+    return S
+
+@app.route('/deleteform')
+def deleteform():
+    S  = "<!DOCTYPE html>\n"
+    S += "<html>\n"
+    S += "  <head>\n"
+    S += "      <title>Entering a value</title>\n"
+    S += style()
+    S += "  </head>\n"
+    S += "  <body>\n"
+    S += "      <h1>Entering a value</h1>\n"
+    S += "      <form action='/delete'>\n"
+    S += "          <input type='text' name='title' value='Affenpinscher'/>\n"
+    S += "          <input type='text' name='link' value='https://commons.wikimedia.org/wiki/File:Affenpinscher_dog.jpg'/>\n"
+    S += "          <input type='text' name='descript' value='Affenpinscher dog'/>\n"
+    S += "          <input type='submit' value='Submit'/>\n"
+    S += "      </form>\n"
+    S += "  </body>\n"
+    S += "</html>\n"
+    return S
+
 @app.route('/')
 def index():
+    S = "<!DOCTYPE html>\n"
+    S += "<html>\n"
+    S += "  <head>\n"
+    S += "      <title>Images of dogs</title>\n"
+    S += style()
+    S += "  </head>\n"
+    S += "  <body>\n"
+    S += "      <h1>Images of dogs</h1>\n"
+    S += "          <form action='/delete'>\n"
+    S += "              <table>\n"
+    for (title, link, descript) in test_table():
+        S += f"             <tr><th>Title</th>\n"
+        S += f"                 <th>Link (click on image)</th>\n"
+        S += f"                 <th>Description</th>\n" 
+        S += f"             </tr>\n"
+        S += f"             <tr><td>{title}</td>\n"
+        S += f"                 <td><a target='blank' href='{link}'><img src='{link}' style='width:150px'></a></td>\n"
+        S += f"                 <td>{descript}</td>\n" 
+        S += f"                 <td><input type='radio' value='{title}' name='title'/></td>\n" 
+        S += f"             </tr>\n"
+    #S += "         <tr><td><input type='submit' name='action' value='Delete'></td>\n"
+    S += "         </tr>\n"
+    S += "              </table>\n"
+    S += "         <input type='submit' name='action' value='Delete'>\n"
+    S += "        </form>\n"
+
+    S += "      <form action='/add'>\n"
+    S += "          <input type='text' name='title' placeholder='Title of the image'/>\n"
+    S += "          <input type='text' name='link' placeholder='Place image link'/>\n"
+    S += "          <input type='text' name='descript' placeholder='Write description'/>\n"
+    S += "          <input type='submit' value='Submit'/>\n"
+    S += "      </form>\n"
+    S += "  </body>\n"
+    S += "</html>\n"
+    return S
+
+@app.route('/index')
+def index_old():
     S  = "<!DOCTYPE html>\n"
     S += "<html>\n"
     S += "  <head>\n"
     S += "      <title>Images of dogs</title>\n"
+    S += style()
     S += "  </head>\n"
     S += "  <body>\n"
     S += "      <h1>Images of dogs </h1>\n"
@@ -64,6 +178,8 @@ def index():
     for (title, link, descript) in test_table():
         S += f"        <li>{title}: {link}: {descript}</li>\n"
     S += "      </ul>\n"
+    S += "      <p><a href='/addform'>Form!</a></p>\n"
+    S += "      <p><a href='/deleteform'>Delete Form!</a></p>\n"
     S += "  </body>\n"
     S += "</html>\n"
     return S
--- db/init.sql
+++ db/init.sql
@@ -10,6 +10,6 @@ CREATE TABLE images (
 INSERT INTO images
     (title, link, descript)
 VALUES
-    ('Chihuahua', 'https://commons.wikimedia.org/wiki/File:3B3F4948-4AFF-4C736FAD5353643Ckobe.jpg', 'Chihuahua puppy'),
-    ('Pomsky', 'https://commons.wikimedia.org/wiki/File:Pomsky_Dog_Breed_-_Pomeranian_Husky_Mix.jpg', 'Pomsky is Mix Breed of Pomeranian and Siberian Husky. Pomsky being the newcomer in the group of designer dogs, there is no doubt that he is one of the most loving dogs you can come across.'),
-    ('Pomeranian', 'https://commons.wikimedia.org/wiki/File:Pomeranian.JPG', 'Pomeranian 18 months old');
+    ('Chihuahua', 'https://upload.wikimedia.org/wikipedia/commons/7/77/3B3F4948-4AFF-4C736FAD5353643Ckobe.jpg', 'Chihuahua puppy'),
+    ('Pomsky', 'https://upload.wikimedia.org/wikipedia/commons/5/54/Pomsky_Dog_Breed_-_Pomeranian_Husky_Mix.jpg', 'Pomsky is Mix Breed of Pomeranian and Siberian Husky. Pomsky being the newcomer in the group of designer dogs, there is no doubt that he is one of the most loving dogs you can come across.'),
+    ('Pomeranian', 'https://upload.wikimedia.org/wikipedia/commons/c/ca/Pomeranian.JPG', 'Pomeranian 18 months old');
